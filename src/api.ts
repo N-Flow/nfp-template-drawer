10dd521fba7a924e1ce47ac65dcb189e09534cc4
YingFeng256 <yf2627181812@gmail.com>
2025-01-16 01:24:10 +0800

    feat: 删除多余引入

--- src/api.ts
+++ src/api.ts
@@ -45,7 +45,6 @@ export let RsdTextarea: FunctionComponent<RsdInputProps>
 export let RsdTitle: FunctionComponent<RsdTitleProps>
 
 export let rss: { [key: string]: string }
-export let scrollbar: { [key: string]: string }
 export let rsbStyles: { [key: string]: string }
 
 export let SpaceReact: typeof React
@@ -80,7 +79,6 @@ export function loadApi() {
   UserService = api.services.main.UserService
 
   rss = api.styles.rss
-  scrollbar = api.styles.scrollbar
   rsbStyles = api.styles.rsbStyles
 
   Anchor = api.components.normal.Anchor
